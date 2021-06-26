2be63aa1d14c318b218afcb9cd15188ef445dee4
RYX <1262077382@qq.com>
2021-06-26 21:52:25 +0800

    <fix> 登录界面重定向次数多问题

--- src/main/java/com/ryx/config/MyAccessDecisionManager.java
+++ src/main/java/com/ryx/config/MyAccessDecisionManager.java
@@ -1,8 +1,12 @@
 package com.ryx.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.RememberMeAuthenticationToken;
@@ -28,6 +32,9 @@ public class MyAccessDecisionManager implements AccessDecisionManager {
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
 //        System.out.println(authentication.getPrincipal().toString());
+
+//        roleHierarchy.getReachableGrantedAuthorities()
+
         for (ConfigAttribute attribute : configAttributes) {
             if ("ROLE_login".equals(attribute.getAttribute())){
                 //判断authentication是否登录  如果是匿名用户
@@ -38,10 +45,11 @@ public class MyAccessDecisionManager implements AccessDecisionManager {
                 }
             }
 
-            //正常登录 正常用户 我们就拿到他的角色 用来决定他是否能访问
+            //正常登录 正常用户 我们就通过数据库拿到他的角色 用来决定他是否能访问
             Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
             for (GrantedAuthority authority : authorities) {
                 //访问的接口 所需要的角色 是我们用户所拥有的角色 直接break; 这里只是单纯具备一个 加入需要具备多个则另外判断
+                // 而且如何解决角色继承问题也需要在这里 因为你即便注入了RoleHierarchy 但是在这里不给予放行
                 if (authority.getAuthority().equals("ROLE_"+attribute.getAttribute())){
                     return;
                 }
--- src/main/java/com/ryx/controller/LoginController.java
+++ src/main/java/com/ryx/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.ryx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @BelongsPackage: com.ryx.controller
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-//    @GetMapping("/loginPage")
-//    public String loginPage(){
-//        return "loginPage";
-//    }
+    @GetMapping("/index")
+    public String loginPage(){
+        return "index";
+    }
 
 }
