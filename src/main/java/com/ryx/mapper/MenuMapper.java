6abde89956d0184d59fd7be5d6526333d2aac2d7
RYX <1262077382@qq.com>
2021-07-02 23:17:04 +0800

    登录+注销的一些小疑惑解决 一般前后端使用json比较多-》handler

---
+++ src/main/java/com/ryx/mapper/MenuMapper.java
@@ -0,0 +1,15 @@
+package com.ryx.mapper;
+
+import com.ryx.entity.Menu;
+
+import java.util.List;
+
+/**
+ * @BelongsPackage: com.ryx.mapper
+ * @Author: 容永轩
+ * @CreateTime: 2021-06-24
+ * @Description:
+ */
+public interface MenuMapper {
+    List<Menu> getAllMenus();
+}
--- src/test/java/com/ryx/SpringSecurityApplicationTests.java
+++ src/test/java/com/ryx/SpringSecurityApplicationTests.java
@@ -4,6 +4,7 @@ import com.ryx.service.MenuService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
@@ -14,6 +15,11 @@ class SpringSecurityApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(menuService.getAllMenus());
+    }
+
+    @Test
+    void contextLoads5(){
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
     }
 
 }
